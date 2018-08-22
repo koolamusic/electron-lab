3f00569ac0da5daf6f6bd8fd5f5c32da81341c32
Andrew <me@andrewmiracle.com>
2018-08-22 21:17:48 +0100

    testing out the electron framework

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+require('electron-reload')(__dirname)
+
+console.log('main.js executing')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -7,10 +10,14 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1200, height: 720})
+
+
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
+
+  console.log('main.js index loaded');
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
@@ -20,7 +27,9 @@ function createWindow () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    mainWindow = null;
+
+    console.log('main window closed');
   })
 }
 
